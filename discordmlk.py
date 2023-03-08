a325e24ecd31dfb6704b3135e18b21097ebbc080
Milkyc <sagaidaan@yandex.ru>
2023-03-08 18:18:50 +0300

    fix connection close error

--- discordmlk.py
+++ discordmlk.py
@@ -1,58 +1,36 @@
-import websocket
-import time
 import threading
-import json
+import websocket
 import requests
+import json
+import time
 
 class Discord:
-    def __init__(self, token, asynchronously=False):
+    def __init__(self, token):
         self.__recieve_message_function = None
-        self.__token = token
-        self.__web_socket_init()
-        if not asynchronously:
-            self.__message_processing()
-        else:
-            threading.Thread(target=self.__message_processing).start()
-        self.__session = requests.Session()
-        self.__session.headers.update({'authorization': self.__token})
+        self.__requests_session_init(token)
+        self.__web_socket_init(token)
 
-    def __web_socket_init(self):
+    def __init_event(self, function, recieve_message_function):
+        return threading.Thread(target=function, args=(recieve_message_function,)).start()
+
+    def __requests_session_init(self, token):
+        self.__session = requests.Session()
+        self.__session.headers.update({
+            'authorization': token
+        })
+
+    def __web_socket_init(self, token):
         self.__ws = websocket.WebSocket()
         self.__ws.connect('wss://gateway.discord.gg/?v=6&encoding=json')
 
-    def __send_json_request(self, goal, request):
-        if not self.__ws.connected:
-            self.__web_socket_init()
-        goal.send(json.dumps(request))
+        event = self.__recieve_json_response()
 
-    def __recieve_json_response(self, goal):
-        if not self.__ws.connected:
-            self.__web_socket_init()
-        response = goal.recv()
-        if response:
-            return json.loads(response)
-
-    def __heartbeat(self, goal, interval):
-        while True:
-            time.sleep(interval)
-            if not self.__ws.connected:
-                self.__web_socket_init()
-            payload = {
-                'op': 1,
-                'd': 'null'
-            }
-            self.__send_json_request(goal, payload)
-
-    def __message_processing(self):
-        ws = self.__ws
-        response = self.__recieve_json_response(ws)
-
-        heartbeat_interval = response['d']['heartbeat_interval'] / 1000
-        threading.Thread(target=self.__heartbeat, args=(ws, heartbeat_interval)).start()
+        heartbeat_inteval = event['d']['heartbeat_interval'] / 1000
+        threading.Thread(target=self.__heartbeat, args=(heartbeat_inteval,)).start()
         payload = {
             'op': 2,
             'd': {
-                'token': self.__token,
+                'token': token,
                 'properties': {
                     '$os': 'linux',
                     '$browser': 'chrome',
@@ -60,19 +38,37 @@ class Discord:
                 }
             }
         }
-        ws.send(json.dumps(payload))
+        self.__send_json_request(payload)
 
+    def __heartbeat(self, interval):
+        print('----Heartbeat begin----')
+        while True:
+            time.sleep(interval)
+            payload = {
+                'op': 1,
+                'd': 'null'
+            }
+            self.__send_json_request(payload)
+            print('----Heartbeat send----')
+
+    def __send_json_request(self, payload):
+        self.__ws.send(json.dumps(payload))
+
+    def __recieve_json_response(self):
+        response = self.__ws.recv()
+        if response:
+            return json.loads(response)
+
+    def __message_processing(self, function):
         while True:
             try:
-                response = json.loads(ws.recv())
-                if self.__recieve_message_function is not None:
-                    self.__recieve_message_function(response)
+                response = json.loads(self.__ws.recv())
+                function(response)
             except:
                 pass
 
-
-    def set_recieve_message_function(self, function):
-        self.__recieve_message_function = function
+    def set_recieve_message_function(self, recieve_message_function):
+        self.__init_event(self.__message_processing, recieve_message_function)
 
     def send_slash_command(self, application_id, guild_id, channel_id, session_id, command_name, user_id):
         command = self.get_application_command(application_id, channel_id, command_name)
@@ -84,14 +80,14 @@ class Discord:
             'channel_id': channel_id,
             'session_id': session_id,
             'data': {
-                    'version': command_version,
-                    'id': command_id,
-                    'name': command_name,
-                    'options': [{
-                        'type': 6,
-                        'name': 'user',
-                        'value': user_id
-                    }]
+                'version': command_version,
+                'id': command_id,
+                'name': command_name,
+                'options': [{
+                    'type': 6,
+                    'name': 'user',
+                    'value': user_id
+                }]
             }, 'type': 2
         }
         return self.__session.post('https://discord.com/api/v9/interactions', json=data)
@@ -106,4 +102,3 @@ class Discord:
         for command in application_commands:
             if command['name'] == command_name:
                 return command
-        return {}
