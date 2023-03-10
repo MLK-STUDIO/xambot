855397d5bb7dc791a184a5ee67975e6307e5e536
Milkyc <sagaidaan@yandex.ru>
2023-03-10 23:03:43 +0300

    The code has been rewritten to a new style

--- xambot.py
+++ xambot.py
@@ -1,18 +1,17 @@
-import discordmlk
+import discordmlk as discordmlk
 
 token = open('token', 'r').read()
 discord = discordmlk.Discord(token)
 
 def main():
-    discord.set_recieve_message_function(recieve_message)
     print('Все системы запущены, жук готов к бою!')
 
+@discord.event
+def on_message(response):
+    print(f'{response["author"]["username"]}: {response["content"]}')
+    univ_content = response['content'].lower().replace(' ', '')
 
-def recieve_message(response):
-    print(f'{response["d"]["author"]["username"]}: {response["d"]["content"]}')
-    univ_content = response['d']['content'].lower().replace(' ', '')
-
-    apply_command = lambda x: discord.send_slash_command('1033663376938770482', '1023529522760532020', '1023540638186217473', 'ca14a31ab72e655413a2e93fffa2be25', x, '792042309503418400')
+    apply_command = lambda x: discord.send_slash_command('1033663376938770482', '1023529522760532020', '1023540638186217473', x, '792042309503418400')
     if univ_content == 'жукпогладьхама':
         apply_command('pat')
     elif univ_content == 'жуккусихама':
@@ -26,8 +25,8 @@ def recieve_message(response):
     elif univ_content == 'жуклюбихама':
         discord.send_message('1023540638186217473', '<@792042309503418400> я тебя :heart:')
 
-    author_id = response['d']['author']['id']
-    apply_command = lambda x: discord.send_slash_command('1033663376938770482', '1023529522760532020', '1023540638186217473', 'ca14a31ab72e655413a2e93fffa2be25', x, f'{author_id}')
+    author_id = response['author']['id']
+    apply_command = lambda x: discord.send_slash_command('1033663376938770482', '1023529522760532020', '1023540638186217473', x, f'{author_id}')
     if univ_content == 'жукпогладьменя':
         apply_command('pat')
     elif univ_content == 'жуккусименя':
